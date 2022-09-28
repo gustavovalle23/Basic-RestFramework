f5ff1e77e035fde2f4cf223a1368fd3163fe9b8d
gustavovalle23 <gustavo.vallerp@hotmail.com>
2022-09-28 00:12:19 -0300

    :hammer: remaking rest framework template

---
+++ auth/admin.py
@@ -0,0 +1,11 @@
+from django.contrib import admin
+
+# class Users(admin.ModelAdmin):
+#     list_display = ('id', 'name', 'document', 'birth_date')
+#     list_display_links = ('id', 'name')
+#     search_fields = ('name',)
+#     list_per_page = 20
+#     ordering = ['id']
+
+
+# admin.site.register(User, Users)
--- setup/urls.py
+++ setup/urls.py
@@ -1,20 +1,22 @@
+"""setup URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/4.0/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
 from django.contrib import admin
 from django.urls import path, include
-from college.views import *
-from rest_framework import routers
-from rest_framework_simplejwt import views as jwt_views
-
-
-router = routers.DefaultRouter()
-router.register('students', StudentsViewSet, basename='Students')
-router.register('courses', CoursesViewSet, basename='Courses')
-router.register('registrations', RegistrationViewSet, basename='Registrations')
-
 
 urlpatterns = [
-    path('', include(router.urls)),
     path('admin/', admin.site.urls),
-    path('student/<int:pk>/registrations/', ListStudentRegistrations.as_view()),
-    path('course/<int:pk>/registrations/', ListRegistrationsStudents.as_view()),
-    path('api/token/', jwt_views.TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('', include('auth.urls'), name='auth')
 ]

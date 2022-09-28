bdad97fbbc1dfc933512252fa384f3b915a9ebac
gustavovalle23 <gustavo.vallerp@hotmail.com>
2022-09-28 20:10:35 -0300

    :sparkles: adding jwt authentication

--- authentication/urls.py
+++ authentication/urls.py
@@ -7,8 +7,6 @@ router = routers.DefaultRouter()
 router.register('users', views.UserViewSet)
 router.register('groups', views.GroupViewSet)
 
-# Wire up our API using automatic URL routing.
-# Additionally, we include login URLs for the browsable API.
 urlpatterns = [
     path('', include(router.urls)),
     path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
